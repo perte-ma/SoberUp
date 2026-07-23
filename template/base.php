948f04f3e7f6ffcac58d10d494dd0afa4b9167b8
Marco Pertegato <marco.pertegato@studio.unibo.it>
2026-07-23 22:52:05 +0200

    Estratta la navbar in un componente a se stante

--- template/base.php
+++ template/base.php
@@ -8,20 +8,7 @@
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">SoberUp</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navMain">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" <?php isActive("index.php"); ?> href="index.php">Home</a></li>
-                    <!-- TODO: aggiungere qui via via i link a Catalogo, Serata, Storico, Amici, Profilo, Login/Logout -->
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php require 'template/navbar.php'; ?>
 
     <main class="container my-4">
     <?php
